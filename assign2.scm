bcaaee3c796826f5e0f79b51b3e8aefb2f48f125
Drew Erny <drewerny@gmail.com>
2014-10-16 10:53:41 -0500

    finished task 1

--- assign2.scm
+++ assign2.scm
@@ -29,11 +29,39 @@
 ;
 
 (define (range start stop step)
+  (define (mod n m) (+ 1 (% (- n 1) m)))
   (define (iter store src)
-    ()
+    (cond 
+      ((< src start) store)
+      (else
+        (iter 
+          (cons src store)
+          (- src step)
+          )
+        )
+      )
+    )
+  (iter nil (- stop (mod (- stop start) step)))
+  )
+
+(define (for-loop arglist procedure)
+  (cond
+    ((null? arglist) nil)
+    (else (begin
+      (procedure (car arglist))
+      (for-loop (cdr arglist) procedure)
+      ))
     )
   )
 
+(define (run1)
+  (for-loop (range 0 5 1) (lambda (i) (print i ", ")))
+  (println)
+  (for-loop (range 1 13 4) (lambda (j) (print j ", ")))
+  (println)
+  (for-loop (range 1 15 4) (lambda (k) (print k ", ")))
+  (println)
+  )
 ;
 ; Task 2
 ;
@@ -53,6 +81,7 @@
   (exprTest ((pval + 1 2 3)) 6)
   )
 
+(run1)
 (run2)
 
 ; Last statement!
